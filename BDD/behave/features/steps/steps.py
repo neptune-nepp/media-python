2c8364d718cb2c0967bb5076baca42053d5b1f6c
Ana Beatriz Schlink <abnschlink@gmail.com>
2024-04-23 10:52:36 -0300

    fixing name media.feature, creating paste feature and file steps.py

--- BDD/behave/features/steps/steps.py
+++ BDD/behave/features/steps/steps.py
@@ -2,12 +2,14 @@ from behave import given, when, then
 
 @given('eu tenho dois números inteiros: {num1:d} e {num2:d}')
 def step_impl(context, num1, num2):
+    if not isinstance(num1, int) or not isinstance(num2, int):
+        raise TypeError("Os números devem ser inteiros")
     context.num1 = num1
     context.num2 = num2
 
 @when('eu somo os dois números e divido por 2')
 def step_impl(context):
-    context.resultado = (context.num1 + context.num2)/2
+    context.resultado = (context.num1 + context.num2) / 2
 
 @then('o resultado deve ser {resultado:d}')
 def step_impl(context, resultado):
--- BDD/steps/__init__.py
+++
@@ -1,1 +0,0 @@
-print("O pacote BDD foi inicializado!")
